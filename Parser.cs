16d2759b088cd47677a21bd5a44b8e556a5078e1
zettazed <70200066+zettazed@users.noreply.github.com>
2022-11-23 20:48:26 +1100

    Update Parser.cs

--- Parser.cs
+++ Parser.cs
@@ -1,7 +1,7 @@
 using System.Linq;
 public static class Parser
 {
-    public static int IntParsing(string text)
+    public static int StringToInt(string text)
     {
         int parsingInt = 0;
         int.TryParse(string.Join("", text.Where(c => char.IsDigit(c))), out parsingInt);
