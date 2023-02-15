b88701acf10a2aa84df0aadca935b449d31fa1b3
zettazed <70200066+zettazed@users.noreply.github.com>
2023-02-15 21:13:35 +1200

    Update DateManager.cs

--- Managers/DateManager.cs
+++ Managers/DateManager.cs
@@ -7,7 +7,7 @@ public static class DateManager
         int currentDay;
         string date = DateTime.Today.ToString();
         string[] parserDate = date.Split('.');
-        currentDay = Parser.IntParsing(parserDate[0]);
+        currentDay = Parser.StringToInt(parserDate[0]);
 
         return currentDay;
     }
@@ -17,7 +17,7 @@ public static class DateManager
         int currentHour;
         string date = DateTime.UtcNow.ToLocalTime().ToString();
         string[] parserDate = date.Split(':');
-        currentHour = Parser.IntParsing(parserDate[0]);
+        currentHour = Parser.StringToInt(parserDate[0]);
 
         return currentHour;
     }
@@ -27,7 +27,7 @@ public static class DateManager
         int currentMinute;
         string date = DateTime.UtcNow.ToLocalTime().ToString();
         string[] parserDate = date.Split(':');
-        currentMinute = Parser.IntParsing(parserDate[1]);
+        currentMinute = Parser.StringToInt(parserDate[1]);
 
         return currentMinute;
     }
@@ -37,7 +37,7 @@ public static class DateManager
         int currentSecond;
         string date = DateTime.UtcNow.ToLocalTime().ToString();
         string[] parserDate = date.Split(':');
-        currentSecond = Parser.IntParsing(parserDate[2]);
+        currentSecond = Parser.StringToInt(parserDate[2]);
 
         return currentSecond;
     }
