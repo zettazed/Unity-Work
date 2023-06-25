a86fad69879e17b01b4051a281ad397984f18c7d
ZettaZed <70200066+zettazed@users.noreply.github.com>
2023-06-26 07:09:38 +1200

    Update VKManager.cs

--- Managers/VKManager.cs
+++ Managers/VKManager.cs
@@ -5,15 +5,43 @@ using System.Runtime.InteropServices;
 
 public class VKManager : MonoBehaviour
 {
+    public static VKManager Instance;
+
+    [DllImport("__Internal")]
+    private static extern void ShowInterstitialRequest();
+
+    [DllImport("__Internal")]
+    private static extern void ShowRewardedRequest();
+
     [DllImport("__Internal")]
     private static extern void ShareRequest();
 
     [DllImport("__Internal")]
     private static extern void FriendsInviteRequest();
 
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void ShowInterstitial()
+    {
+        ShowInterstitialRequest();
+    }
+
+    public void RewardVK()
+    {
+        AdsManager.Instance.Yandex_rewardedAd_OnUserEarnedReward();
+    }
+
+    public void ShowRewarded()
+    {
+        ShowRewardedRequest();
     }
 
     public void Share()
@@ -28,12 +56,12 @@ public class VKManager : MonoBehaviour
 
     public void GiveMoney1000()
     {
-        Game.Instance.gameData.AddMoney(1000);
+        //Game.Instance.gameData.AddMoney(1000);
     }
 
     public void GiveMoney5000()
     {
-        Game.Instance.gameData.AddMoney(5000);
+        //Game.Instance.gameData.AddMoney(5000);
     }
 
     public void AudioOff()
