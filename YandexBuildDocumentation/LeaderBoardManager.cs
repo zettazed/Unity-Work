694ba3b00af3407bc9a41c6db7fcb15464b0cf25
ZettaZed <70200066+zettazed@users.noreply.github.com>
2024-02-01 05:27:11 +0300

    Add files via upload

---
+++ YandexBuildDocumentation/LeaderBoardManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using YG;
+
+public class LeaderBoardManager : MonoBehaviour
+{
+    public static LeaderBoardManager Instance;
+
+    [SerializeField]
+    private LeaderboardYG leaderboardYG;
+
+    public void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void NewScore(int score)
+    {
+        // —татический метод добавление нового рекорда
+        YandexGame.NewLeaderboardScores(leaderboardYG.nameLB, score);
+
+        // ћетод добавление нового рекорда обращением к компоненту LeaderboardYG
+        // leaderboardYG.NewScore(int.Parse(scoreLbInputField.text));
+    }
+}
