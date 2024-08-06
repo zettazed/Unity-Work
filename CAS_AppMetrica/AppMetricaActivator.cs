e91d1024f6c834141051d34f4a7979a7bd951d9a
ZettaZed <70200066+zettazed@users.noreply.github.com>
2024-08-06 18:33:43 +0300

    Add files via upload

---
+++ CAS_AppMetrica/AppMetricaActivator.cs
@@ -0,0 +1,22 @@
+using Io.AppMetrica;
+using UnityEngine;
+
+public static class AppMetricaActivator
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Activate()
+    {
+        AppMetrica.Activate(new AppMetricaConfig("APIKey")
+        {
+            FirstActivationAsUpdate = !IsFirstLaunch(),
+        });
+    }
+
+    private static bool IsFirstLaunch()
+    {
+        // Implement logic to detect whether the app is opening for the first time.
+        // For example, you can check for files (settings, databases, and so on),
+        // which the app creates on its first launch.
+        return true;
+    }
+}
