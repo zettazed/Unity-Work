fcc3595c403307bb120de4da66ca190a5f61d305
ZettaZed <70200066+zettazed@users.noreply.github.com>
2024-08-06 21:42:51 +0300

    Add files via upload

---
+++ CAS_AppMetrica/AdManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+using CAS.AdObject;
+
+public class AdManager : MonoBehaviour
+{
+    public static AdManager Instance;
+
+    public UnityEvent InterstitialAdShow;
+    public UnityEvent RewardedAdShow;
+
+    public Action OnInterstitialClosed;
+    public Action OnRewardEvent;
+
+    public bool secondFortune = false;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ShowInterstitial()
+    {
+        InterstitialAdShow.Invoke();
+    }
+
+    public void ShowRewarded()
+    {
+        RewardedAdShow.Invoke();
+    }
+
+    public void OnReward()
+    {
+        OnRewardEvent.Invoke();
+    }
+
+    public void OnInterstitialClose()
+    {
+        OnInterstitialClosed.Invoke();
+    }
+}
